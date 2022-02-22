89d0c0d40e56d6b81e17cf370514a0dce2626e9e
Lorenas Bartkus <lorenas.bartkus@deeper.eu>
2022-02-22 08:08:51 +0200

    Update interactor template

--- VIP.xctemplate/___FILEBASENAME___Interactor.swift
+++ VIP.xctemplate/___FILEBASENAME___Interactor.swift
@@ -5,12 +5,12 @@ protocol ___VARIABLE_productName___BusinessLogic {
 }
 
 protocol ___VARIABLE_productName___Interacting {
-    var presenter: ___VARIABLE_productName___BusinessLogic! { get set }
+    var presenter: ___VARIABLE_productName___PresentationLogic! { get set }
 }
 
 final class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_productName___Interacting {
     // MARK: - Variables
-    var presenter: ___VARIABLE_productName___BusinessLogic!
+    var presenter: ___VARIABLE_productName___PresentationLogic!
 }
 
 extension ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_productName___BusinessLogic {
